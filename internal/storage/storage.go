5e6c2194d33e108267f8fdf18464dfdd3c97914a
Kapeland <mdidkovskij@gmail.com>
2025-02-11 11:51:31 +0300

    Added storage (postgres only).

---
+++ internal/storage/storage.go
@@ -0,0 +1,25 @@
+package storage
+
+import (
+	"context"
+	"github.com/Kapeland/task-Avito/internal/storage/db"
+)
+
+type PostgresStorage struct {
+	DB *db.PgDatabase
+}
+
+func NewPostgresStorage(ctx context.Context) (PostgresStorage, error) {
+	var dbStorage PostgresStorage
+	database, err := db.NewPostgres(ctx)
+	if err != nil {
+		return PostgresStorage{}, err
+	}
+	dbStorage.DB = database
+	return dbStorage, nil
+}
+
+func (s *PostgresStorage) Close() error {
+	err := s.DB.Close()
+	return err
+}
