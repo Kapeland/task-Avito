da8abd064f1ff8c15e51547381def2f82eb9eef4
Kapeland <mdidkovskij@gmail.com>
2025-02-16 19:19:28 +0300

    Fix linter problems and new wrappers.

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -1,12 +1,13 @@
 package logger
 
 import (
-	"github.com/Kapeland/task-Avito/internal/utils/config"
-	slogmulti "github.com/samber/slog-multi"
-	slogsampling "github.com/samber/slog-sampling"
 	"log/slog"
 	"os"
 	"strings"
+
+	"github.com/Kapeland/task-Avito/internal/utils/config"
+	slogmulti "github.com/samber/slog-multi"
+	slogsampling "github.com/samber/slog-sampling"
 )
 
 var logger *Logger
@@ -28,16 +29,32 @@ func (lgr Logger) Info(msg, tp, method, after string) {
 	lgr.Logger.Info(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
 }
 
+func (lgr Logger) InfoMsg(msg string, args ...any) {
+	lgr.Logger.Info(msg, args...)
+}
+
 func (lgr Logger) Debug(msg, tp, method, after string) {
 	lgr.Logger.Debug(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+}
+
+func (lgr Logger) DebugMsg(msg string, args ...any) {
+	lgr.Logger.Debug(msg, args...)
 }
 
 func (lgr Logger) Warn(msg, tp, method, after string) {
 	lgr.Logger.Warn(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
 }
 
+func (lgr Logger) WarnMsg(msg string, args ...any) {
+	lgr.Logger.Warn(msg, args...)
+}
+
 func (lgr Logger) Error(msg, tp, method, after string) {
 	lgr.Logger.Error(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+}
+
+func (lgr Logger) ErrorMsg(msg string, args ...any) {
+	lgr.Logger.Error(msg, args...)
 }
 
 func CreateLogger(cfg *config.Config) Logger {
